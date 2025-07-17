326e2079510f3d716c6b2427463dba75bd4d63f8
ramesh12331 <mamidiramesh503@gmail.com>
2025-07-17 11:47:37 +0530

    videoCard

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -4,7 +4,7 @@ import VideoContainer from './VideoContainer'
 
 const MainContainer = () => {
   return (
-    <div className='mt-14 pt-4 px-4 w-[83%]'>
+    <div className='flex-1 mt-14 pt-4 px-4 w-[83%]'>
       <ButtonList/>
       <VideoContainer/>
     </div>
--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import VideoCard from "./VideoCard";
 
 const VideoContainer = () => {
   return (
-   <div className='max-h-screen'>
-    Video Container
+    <div className="flex max-h-screen overflow-y-scroll scrollbar-hide flex-wrap gap-4 justify-center">
+      {/* Video Container */}
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
     </div>
-  )
-}
+  );
+};
 
-export default VideoContainer
+export default VideoContainer;
