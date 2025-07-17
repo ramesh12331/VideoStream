47bd4bff7895f9d802a69b315c969b2c6e7925f0
ramesh12331 <mamidiramesh503@gmail.com>
2025-07-18 01:19:01 +0530

    search suggestions

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -13,7 +13,7 @@ const VideoContainer = () => {
   const getVideos = async() =>{
     const data = await fetch(YOUTUBE_VIDEO_API);
     const json = await data.json();
-    console.log(json.items);
+    // console.log(json.items);
     setVideos(json.items)
   }
   // getVideos()
@@ -25,6 +25,8 @@ const VideoContainer = () => {
           <VideoCard info={video} />
         </Link>
       ))}
+      
+      {/* <VideoCard info={videos[0]} /> */}
     </div>
   );
 };
